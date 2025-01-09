50f992a093621a51d554426f901b58ced842b0d6
daniel-wang-design <daniel.wang2@uwaterloo.ca>
2025-01-09 18:56:28 -0500

    add cuda support

--- neural_networks/number_recognition.py
+++ neural_networks/number_recognition.py
@@ -15,8 +15,8 @@ EVALSET_PATH = 'number_recognition_evalset'
 
 trainset = datasets.MNIST(TRAINSET_PATH, download=True, train=True, transform=transform)
 evalset = datasets.MNIST(EVALSET_PATH, download=True, train=False, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
-evalloader = torch.utils.data.DataLoader(evalset, batch_size=64, shuffle=True)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True, num_workers=8, pin_memory=True)
+evalloader = torch.utils.data.DataLoader(evalset, batch_size=64, shuffle=True, num_workers=8, pin_memory=True)
 
 input_size = 784
 hidden_sizes = [128, 64]
@@ -28,22 +28,29 @@ model = nn.Sequential(nn.Linear(input_size, hidden_sizes[0]),
                       nn.ReLU(),
                       nn.Linear(hidden_sizes[1], output_size),
                       nn.LogSoftmax(dim=1))
-print(model)
 
+print("Training the following model:")
+print(model)
+print("*******************************")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Training model on {device}")
+print("*******************************")
+
+model = model.to(device=device)
 
 criterion = nn.NLLLoss()
-images, labels = next(iter(trainloader))
-images = images.view(images.shape[0], -1)
-
-
 optimizer = optim.SGD(model.parameters(), lr=0.003, momentum=0.9)
+
 time0 = time()
-epochs = 15
-for e in range(epochs):
+EPOCHS = 50
+
+for e in range(EPOCHS):
     running_loss = 0
     for images, labels in trainloader:
-        images = images.view(images.shape[0], -1)
-    
+        images = images.view(images.shape[0], -1).to(device)
+
+        labels = labels.to(device)
+        
         optimizer.zero_grad()
         
         output = model(images)
@@ -55,20 +62,21 @@ for e in range(epochs):
         
         running_loss += loss.item()
     else:
-        print("Epoch {} - Training loss: {}".format(e, running_loss/len(trainloader)))
+        print("Epoch {} - Training loss: {}".format(e, running_loss / len(trainloader)))
         
-print("\nTraining Time (minutes) =",(time()-time0)/60)
+print("\nTraining Time (minutes) =",(time() - time0) / 60)
 
 correct_count, all_count = 0, 0
 for images,labels in evalloader:
   for i in range(len(labels)):
-    img = images[i].view(1, 784)
+    img = images[i].view(1, 784).to(device)
     with torch.no_grad():
         logps = model(img)
 
     
     ps = torch.exp(logps)
-    probab = list(ps.numpy()[0])
+    ps = ps.cpu()
+    probab = list(ps.numpy(force=True)[0])
     pred_label = probab.index(max(probab))
     true_label = labels.numpy()[i]
     
